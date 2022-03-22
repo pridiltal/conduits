658cedcf286f3bc1f352f13dfd07bd817668b6d6
Priyanga Talagala <pritalagala@gmail.com>
2022-03-23 01:36:33 +0530

    update normlaize function

--- R/normalize.R
+++ R/normalize.R
@@ -10,33 +10,34 @@
 #' which are uniquely identified by the corresponding
 #' Timestamp.
 #' @return A \code{\link[tibble]{tibble}} with the conditional normliased series
-#' @importFrom dplyr mutate bind_cols
-#' @importFrom stats predict qnorm
-#' @importFrom broom augment
+#' @importFrom dplyr ensym pull
+#' @importFrom mgcv predict.gam
 #' @examples
 #' data <- NEON_PRIN_5min_cleaned %>%
-#' dplyr::filter(site == "upstream") %>%
-#' dplyr::select(Timestamp, turbidity, level, conductance, temperature)
+#'   dplyr::filter(site == "upstream") %>%
+#'   dplyr::select(Timestamp, turbidity, level, conductance, temperature)
 #'
 #' fit_mean <- data %>%
-#' conditional_mean(turbidity ~ s(level, k = 8 ) +
-#' s(conductance, k = 8) + s(temperature, k = 8))
+#'   conditional_mean(turbidity ~ s(level, k = 8) +
+#'     s(conductance, k = 8) + s(temperature, k = 8))
 #'
-#' #fit_var <- data %>%
-#' #conditional_var(turbidity ~ level + conductance + temperature, fit_mean,
-#' #knots = c(7,7,7), family = "Gamma")
+#' fit_var <- data %>%
+#'   conditional_var(
+#'     turbidity ~ s(level, k = 7) + s(conductance, k = 7) + s(temperature, k = 7),
+#'     family = "Gamma",
+#'     fit_mean
+#'   )
 #'
-#' #y_norm <-  data %>% normalize( turbidity, fit_mean, fit_var)
-#'
+#' y_norm <- data %>% normalize(turbidity, fit_mean, fit_var)
 #' @export
 #'
-normalize <- function(data, y, fit_mean, fit_var){
-
-  y <- dplyr::enquo(y)
+normalize <- function(data, y, fit_mean, fit_var) {
+  y <- dplyr::ensym(y)
   cond_EY <- as.numeric(mgcv::predict.gam(fit_mean, newdata = data))
-  cond_VY <- as.numeric(mgcv::predict.gam(fit_var, newdata = data,
-                                           type = "response"))
-  y_norm <- (data[y] - cond_EY)/ sqrt(cond_VY)
+  cond_VY <- as.numeric(mgcv::predict.gam(fit_var,
+    newdata = data,
+    type = "response"
+  ))
+  y_norm <- (data %>% dplyr::pull({{ y }}) - cond_EY) / sqrt(cond_VY)
   return(y_norm)
 }
-
